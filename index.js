01862c2862568b549f5caa264ee7a319b5e910db
unknown <ldhung9901@gmail.com>
2020-06-29 02:16:59 +0700

    two

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL, {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(cookieParser("sdfsdfsdf23"));
 app.use(bodyParser.urlencoded({ extended: true }));
